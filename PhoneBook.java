77ef2ecd26466f8c37c0f971b2c5733783d64a86
Edo <37080671+diamante0018@users.noreply.github.com>
2019-12-07 17:58:04 +0000

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,65 +1,50 @@
-import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.Scanner;
 import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.HashMap;
 
-public class PhoneBook {
+public class ReadPhoneBook {
+    private HashMap<String, Long> phoneBook;
 
-	private HashMap<String, Long> phoneBook;
-	
-	public PhoneBook(int BookSize) {
-		phoneBook = new HashMap<String,Long>(BookSize);
-	}
-	
-	public void describe() {
+    public ReadPhoneBook(int BookSize) {
+        phoneBook = new HashMap<String, Long>(BookSize);
+    }
+
+    public void read(String fileName) {
+        try {
+            FileInputStream fileIn = new FileInputStream(fileName + ".ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            phoneBook = (HashMap<String,Long>) in.readObject();
+            in.close();
+            fileIn.close();
+         } 
+         catch (IOException i) {
+            i.printStackTrace();
+            return;
+         } 
+         catch (ClassNotFoundException c) {
+            System.out.println("PhoneBook class not found");
+            c.printStackTrace();
+            return;
+       }
+    }
+
+    public void describe() {
 		System.out.println("The phone book has " + phoneBook.size() + " numbers saved");
 		
 		for (Entry<String, Long> entry : phoneBook.entrySet()) {
 		    System.out.println("The name is: " + entry.getKey() + " with the number: " + entry.getValue());
 		}
 	}
-	
-	public void add (String name, Long number) {
-		phoneBook.put(name, number);
-	}
 
-	public void save() {
-		try {
-			FileOutputStream fileOut = new FileOutputStream("phonebook.ser");
-			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(phoneBook);
-			out.close();
-			fileOut.close();
-		 } 
-		 catch (IOException e) {
-			e.printStackTrace();
-		 }
-	}
-	
-	public static void main(String[] args) {
-		if (args.length<1) {
+    public static void main(String [] args) {
+        if (args.length<2) {
 			System.out.println("Insufficient amount of arguments");
 			System.exit(0);
-		}
-		
-		PhoneBook book = new PhoneBook(Integer.parseInt(args[0]));
-		Scanner input = new Scanner(System.in);
-		String name;
-		Long number;
-		
-		for (int i=0; i<Integer.parseInt(args[0]);i++) {
-			System.out.println("Enter the name: ");
-			name = input.nextLine();
-			
-			System.out.println("Enter the number: ");
-			number = input.nextLong();
-			input.nextLine();
-			book.add(name,number);
-		}
-		
-		input.close();
-		book.describe();
-		book.save();
-	}
-
+        }
+        
+        ReadPhoneBook read = new ReadPhoneBook(Integer.parseInt(args[1]));
+        read.read(args[0]);
+        read.describe();
+    }
 }
